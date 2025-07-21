c7b66d63f1eb6c2e9ee979c30b40d4c8df7cc8b9
Nishant Maharaj <npmaharaj1@protonmail.com>
2025-07-21 12:02:35 +1000

    bug fixes

--- systems/moustachemachine/configuration.nix
+++ systems/moustachemachine/configuration.nix
@@ -12,7 +12,7 @@
   gamingmode.enable = true;
 
   steamModule.enable = lib.mkIf config.gamingmode.enable true;
-  # plasmaModule.enable = lib.mkIf config.gamingmode.enable true;
+  plasmaModule.enable = lib.mkIf config.gamingmode.enable true;
 
 	hardware.graphics = {
 		enable = true;
