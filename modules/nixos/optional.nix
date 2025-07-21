c5869b996ca0be9784e6e9afe20e6e4176a28903
Nishant Maharaj <npmaharaj1@protonmail.com>
2025-07-21 18:54:56 +1000

    test move

--- modules/nixos/optional.nix
+++ modules/nixos/optional.nix
@@ -9,4 +9,7 @@
   options = {
     gamingMode.enable = lib.mkEnableOption "enables gamingMode";
   };
+
+  steamModule.enable = lib.mkIf config.gamingMode.enable true;
+  plasmaModule.enable = lib.mkIf config.gamingMode.enable true;
 }
--- systems/moustachemachine/configuration.nix
+++ systems/moustachemachine/configuration.nix
@@ -11,8 +11,6 @@
 
   gamingMode.enable = true;
 
-  steamModule.enable = lib.mkIf config.gamingMode.enable true;
-  plasmaModule.enable = lib.mkIf config.gamingMode.enable true;
   
 	hardware.graphics = {
 		enable = true;
