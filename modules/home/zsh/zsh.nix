4b53fef4d7546fe847a461344e443b041a10f9ea
Nishant Maharaj <npmaharaj1@protonmail.com>
2025-07-26 10:47:33 +1000

    automated commit

--- modules/home/zsh/zsh.nix
+++ modules/home/zsh/zsh.nix
@@ -11,9 +11,9 @@
       mkdir = "mkdir -p";
       wheel = " sudo usb_modeswitch -v 046d -p c26d -M 0f00010142 -C 0x03 -m 01 -r 01  ";
       vim = "nvim";
-      nurse = "nh os switch && ~/Projects/nixos/modules/home/templates/start.sh";
-      dev-commit = "export CURRENTDIR=$(pwd) && cd ~/Projects/nixos && git add . && git commit -m 'automated commit'; git push origin master; cd ~/Projects/schoolstuff-nix && git add . && git commit -m 'automated commit'; git push origin master; cd ~/.config/nvim && git add . && git commit -m 'automated commit'; git push origin master; cd $CURRENTDIR";
-      dev-pull = "export CURRENTDIR=$(pwd) && cd ~/Projects/nixos && git pull; cd ~/Projects/schoolstuff-nix && git pull; cd ~/.config/nvim && git pull; cd $CURRENTDIR";
+      dev-commit = "export CURRENTDIR=$(pwd) && cd ~/Projects/nixos && git add . && git commit -m 'automated commit'; git push origin master; cd ~/Projects/school-stuff && git add . && git commit -m 'automated commit'; git push origin master; cd ~/.config/nvim && git add . && git commit -m 'automated commit'; git push origin master; cd $CURRENTDIR";
+      dev-pull = "export CURRENTDIR=$(pwd) && cd ~/Projects/nixos && git pull origin master; cd ~/Projects/school-stuff && git pull origin master; cd ~/.config/nvim && git pull origin master; cd $CURRENTDIR";
+      nurse = "dev-pull && nh os switch && ~/Projects/nixos/modules/home/templates/start.sh";
 
     };
 
