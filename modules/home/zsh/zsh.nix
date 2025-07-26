a7caf0b02d8903e05cc88955a72218aeb8a03a70
Nishant Maharaj <npmaharaj1@protonmail.com>
2025-07-26 10:23:48 +1000

    automated commit

--- modules/home/zsh/zsh.nix
+++ modules/home/zsh/zsh.nix
@@ -12,7 +12,7 @@
       wheel = " sudo usb_modeswitch -v 046d -p c26d -M 0f00010142 -C 0x03 -m 01 -r 01  ";
       vim = "nvim";
       nurse = "nh os switch && ~/Projects/nixos/modules/home/templates/start.sh";
-      dev-commit = "export CURRENTDIR=$(pwd) && cd ~/Projects/nixos && git add . && git commit -m 'automated commit' && git push origin master && cd ~/Projects/school-stuff && git add . && git commit -m 'automated commit' && git push origin master && cd ~/.config/nvim && git add . && git commit -m 'automated commit' && git push origin master && cd $CURRENTDIR";
+      dev-commit = "export CURRENTDIR=$(pwd) && cd ~/Projects/nixos && git add . && git commit -m 'automated commit'; git push origin master; cd ~/Projects/school-stuff && git add . && git commit -m 'automated commit'; git push origin master; cd ~/.config/nvim && git add . && git commit -m 'automated commit'; git push origin master; cd $CURRENTDIR";
 
     };
 
