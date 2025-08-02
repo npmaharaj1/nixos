1634f8a21b92e4eb22e18f88f7719238179bd85e
Nishant Maharaj <npmaharaj1@protonmail.com>
2025-08-02 13:32:47 +1000

    indent

--- modules/home/zsh/zsh.nix
+++ modules/home/zsh/zsh.nix
@@ -1,27 +1,25 @@
 { config, pkgs, ... }:
 
 {
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
+    programs.zsh = {
+        enable = true;
+        enableCompletion = true;
 
-    shellAliases = {
-      ls = "ls -a --color=auto";
-      lsl = "tree";
-      mkdir = "mkdir -p";
-      wheel = " sudo usb_modeswitch -v 046d -p c26d -M 0f00010142 -C 0x03 -m 01 -r 01  ";
-      vim = "nvim";
-      dev-commit = "git add . && git commit -m 'automated commit' && git push origin master";
-      nurse = "dev-pull && nh os switch && ~/Projects/nixos/modules/home/templates/start.sh";
+        shellAliases = {
+            ls = "ls -a --color=auto";
+            lsl = "tree";
+            mkdir = "mkdir -p";
+            wheel = " sudo usb_modeswitch -v 046d -p c26d -M 0f00010142 -C 0x03 -m 01 -r 01  ";
+            vim = "nvim";
+            dev-commit = "git add . && git commit -m 'automated commit' && git push origin master";
+            nurse = "dev-pull && nh os switch && ~/Projects/nixos/modules/home/templates/start.sh";
+        };
 
+        sessionVariables = {
+            NH_FLAKE = "/home/nishant/Projects/nixos/";
+            XDG_CONFIG_HOME = "/home/nishant/.config/";
+        };
+
+        envExtra = "wal -i $(cat /home/nishant/.cache/swww/*) -q";
     };
-
-    sessionVariables = {
-      NH_FLAKE = "/home/nishant/Projects/nixos/";
-      XDG_CONFIG_HOME = "/home/nishant/.config/";
-    };
-
-    envExtra = "wal -i $(cat /home/nishant/.cache/swww/*) -q";
-  };
 }
-
