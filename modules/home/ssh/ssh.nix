8443d6adecd0e0dd257efe6ed6514153295e689f
Nishant Maharaj <npmaharaj1@protonmail.com>
2025-10-11 10:53:07 +1100

    automated commit

--- modules/home/ssh/ssh.nix
+++ modules/home/ssh/ssh.nix
@@ -6,47 +6,36 @@
         enable = true;
 
         extraConfig = ''
-      # HOST GITHUB.COM
-      #   HOSTNAME SSH.GITHUB.COM
-      #   USER NPMAHARAJ1 
-      #   PORT 443
-      #   IDENTITYFILE ~/.SSH/ID_RSA-GITHUB
-      #   IdentitiesOnly yes
-      #
-      # Host proxmox
-      #   Hostname 10.1.1.196
-      #   User root
-      #   Port 22
-      #   IdentityFile ~/.ssh/id_rsa-proxmox
-      #   IdentitiesOnly yes
-      #
-      # Host alpine
-      #   Hostname 10.1.1.140
-      #   User root
-      #   Port 22
-      #   IdentityFile ~/.ssh/id_rsa-lightweight
-      #   IdentitiesOnly yes
-      #
-      # Host debian
-      #   Hostname 10.1.1.157
-      #   User root
-      #   Port 22
-      #   IdentityFile ~/.ssh/id_rsa-debian
-      #   IdentitiesOnly yes
-      #
-      # Host rpi
-      #   Hostname 10.176.120.160
-      #   User nishant
-      #   Port 22
-      #   IdentityFile ~/.ssh/id_rsa-rpi
-      #   IdentitiesOnly yes
-      #
-      # Host uptime
-      #   Hostname 10.1.1.172
-      #   User nishant
-      #   Port 22
-      #   IdentityFile ~/.ssh/id_rsa-uptime
-      #   IdentitiesOnly yes
+            Host proxmox
+                Hostname 10.1.1.196
+                User root
+                Port 22
+            
+            Host pbs
+                Hostname 10.1.1.126
+                User root
+                Port 22
+
+            Host rpi
+                Hostname 10.1.1.174
+                User root
+                Port 22
+
+            Host pve
+                Hostname 10.1.1.196
+                User root
+                Port 22
+
+            Host omv
+                Hostname 10.1.1.160
+                User root
+                Port 22
+
+            Host haos
+                Hostname 10.1.1.89
+                User root
+                Port 22
+
 
         '';
     };
