18c02e555d65b1f81f7870bff708411247b54342
Nishant Maharaj <npmaharaj1@protonmail.com>
2026-03-11 20:35:49 +1100

    automated commit

--- flake.nix
+++ flake.nix
@@ -1,82 +1,92 @@
 {
-  description = "A simple NixOS flake";
+    description = "A simple NixOS flake";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
 
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+        nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-  };
-
-  outputs = { nixpkgs, home-manager, nixpkgs-unstable, ... } @ inputs: 
-  let
-    username = "nishant";
-    system="x86_64-linux";
-  in
-  {
-# System Stuff
-    nixosConfigurations = {
-
-      portablemoustachemachine = nixpkgs.lib.nixosSystem {
-        specialArgs = let
-            system = "x86_64-linux";
-        in {
-            inherit inputs;
-            pkgs-stable = import nixpkgs {
-                inherit system;
-                config.allowUnfree = true;
-            };
-
-            pkgs-unstable = import nixpkgs-unstable {
-                inherit system;
-                config.allowUnfree = true;
-            };
+        home-manager = {
+            url = "github:nix-community/home-manager/release-25.11";
+            inputs.nixpkgs.follows = "nixpkgs";
         };
 
-        modules = [
-          ./systems/portablemoustachemachine/configuration.nix
+    };
 
-        home-manager.nixosModules.home-manager {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
+    outputs = { nixpkgs, home-manager, nixpkgs-unstable, ... } @ inputs: 
+        let
+            username = "nishant";
+            system="x86_64-linux";
+        in
+            {
+            # System Stuff
+            nixosConfigurations = {
 
-          home-manager.users.nishant = import ./systems/parent-home.nix;
-        }
-        ];
-      };
+                portablemoustachemachine = nixpkgs.lib.nixosSystem {
+                    specialArgs = let
+                        system = "x86_64-linux";
+                    in {
+                        inherit inputs;
+                        pkgs-stable = import nixpkgs {
+                            inherit system;
+                            config.allowUnfree = true;
+                        };
 
-      moustachemachine = nixpkgs.lib.nixosSystem {
-        specialArgs = let
-            system = "x86_64-linux";
-        in {
-            inherit inputs;
-            pkgs-stable = import nixpkgs {
-                inherit system;
-                config.allowUnfree = true;
-            };
+                        pkgs-unstable = import nixpkgs-unstable {
+                            inherit system;
+                            config.allowUnfree = true;
+                        };
+                    };
 
-            pkgs-unstable = import nixpkgs-unstable {
-                inherit system;
-                config.allowUnfree = true;
+                    modules = [
+                        { nixpkgs = { inherit system; config.allowUnfree = true; }; }
+                        ./systems/portablemoustachemachine/configuration.nix
+
+                        home-manager.nixosModules.home-manager {
+                            home-manager.useGlobalPkgs = true;
+                            home-manager.useUserPackages = true;
+
+                            home-manager.users.nishant = import ./systems/parent-home.nix;
+                        }
+                    ];
+                };
+
+                moustachemachine = nixpkgs.lib.nixosSystem {
+                    specialArgs = let
+                        system = "x86_64-linux";
+                        nixpkgsConfig = {
+                            allowUnfree = true; 
+                        };
+                    in {
+                        inherit inputs;
+                        pkgs-stable = import nixpkgs {
+                            inherit system;
+                            config.allowUnfree = true;
+                        };
+
+                        pkgs-unstable = import nixpkgs-unstable {
+                            inherit system;
+                            config.allowUnfree = true;
+                        };
+
+                        # pkgs = import nixpkgs {
+                        #     inherit system;
+                        #     config.allowUnfree = true;
+                        # };
+                    };
+
+                    modules = [
+                        { nixpkgs = { inherit system; config.allowUnfree = true; }; }
+                        ./systems/moustachemachine/configuration.nix
+
+                        home-manager.nixosModules.home-manager {
+                            home-manager.useGlobalPkgs = true;
+                            home-manager.useUserPackages = true;
+
+                            home-manager.users.nishant = import ./systems/parent-home.nix;
+                        }
+                    ];
+                };
             };
         };
-
-        modules = [
-          ./systems/moustachemachine/configuration.nix
-
-        home-manager.nixosModules.home-manager {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-
-          home-manager.users.nishant = import ./systems/parent-home.nix;
-        }
-        ];
-      };
-    };
-  };
 }
